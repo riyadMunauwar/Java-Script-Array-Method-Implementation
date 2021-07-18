da71626c9aaec7117ae36bfdedd8c6aa13d2766b
riyad <hifazakhatun1108@gmail.com>
2021-07-18 13:11:52 +0600

    reduce and reduceRight function are added

---
+++ ReduceRight.js
@@ -0,0 +1,18 @@
+let name = 'Riyad Munauwar'.split('');
+
+let revName = reduce(name, (acc, item) => acc + item, ' ');
+
+console.log(revName);
+
+function reduce(source, callback, initValue) {
+  if(source && callback && typeof callback === 'function') {
+    var accumulator = initValue || 0;
+
+    for(var i =  source.length - 1; i >= 0; i--){
+      accumulator = callback(accumulator, source[i], i, source);
+    }
+
+    return accumulator;
+  }
+}
+
